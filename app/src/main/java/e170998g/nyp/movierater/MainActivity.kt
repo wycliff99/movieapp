2bd1d8871ca6448f14fff7137049386e1857edba
wycliff99 <wycliff_lee@hotmail.com>
2018-11-14 14:11:19 +0800

    Updated Movieapp

--- app/src/main/java/e170998g/nyp/movierater/MainActivity.kt
+++ app/src/main/java/e170998g/nyp/movierater/MainActivity.kt
@@ -2,11 +2,20 @@ package e170998g.nyp.movierater
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun Add_Movie(v: View){
+
+        Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show()
     }
 }
